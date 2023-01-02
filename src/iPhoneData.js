9d21b825a988af54851698927df3be1950a0f7be
Wivon <brousselilian6@gmail.com>
2023-01-02 15:41:59 +0100

    ✅feat: Refurbished iPhone Prices added

--- src/iPhoneData.js
+++ src/iPhoneData.js
@@ -5,7 +5,8 @@ export const iPhoneInfos = {
         "back-glass": 120,
         "back-camera": 200,
         "id": 500,
-        "logic-board": 500
+        "logic-board": 500,
+        "RLP": 1200
     },
     "iPhone 14 Pro": {
         "screen": 500,
@@ -13,7 +14,8 @@ export const iPhoneInfos = {
         "back-glass": 120,
         "back-camera": 160,
         "id": 500,
-        "logic-board": 500
+        "logic-board": 500,
+        "RLP": 1000
     },
     "iPhone 14 Plus": {
         "screen": 370,
@@ -21,7 +23,8 @@ export const iPhoneInfos = {
         "back-glass": 200,
         "back-camera": 100,
         "id": 500,
-        "logic-board": 500
+        "logic-board": 500,
+        "RLP": 920
     },
     "iPhone 14": {
         "screen": 370,
@@ -29,7 +32,8 @@ export const iPhoneInfos = {
         "back-glass": 180,
         "back-camera": 80,
         "id": 500,
-        "logic-board": 500
+        "logic-board": 500,
+        "RLP": 600
     },
     "iPhone 13 Pro Max": {
         "screen": 500,
@@ -37,7 +41,8 @@ export const iPhoneInfos = {
         "back-glass": 210,
         "back-camera": 105,
         "id": 500,
-        "logic-board": 500
+        "logic-board": 500,
+        "RLP": 750
     },
     "iPhone 13 Pro": {
         "screen": 350,
@@ -45,7 +50,8 @@ export const iPhoneInfos = {
         "back-glass": 210,
         "back-camera": 105,
         "id": 500,
-        "logic-board": 500
+        "logic-board": 500,
+        "RLP": 660
     },
     "iPhone 13": {
         "screen": 220,
@@ -53,7 +59,8 @@ export const iPhoneInfos = {
         "back-glass": 150,
         "back-camera": 65,
         "id": 500,
-        "logic-board": 500
+        "logic-board": 500,
+        "RLP": 550
     },
     "iPhone 13 mini": {
         "screen": 280,
@@ -61,7 +68,8 @@ export const iPhoneInfos = {
         "back-glass": 190,
         "back-camera": 65,
         "id": 500,
-        "logic-board": 500
+        "logic-board": 500,
+        "RLP": 550
     }, 
     "iPhone SE 2022": {
         "screen": 50,
@@ -69,7 +77,8 @@ export const iPhoneInfos = {
         "back-glass": 15,
         "back-camera": 40,
         "id": 200,
-        "logic-board": 200
+        "logic-board": 200,
+        "RLP": 200
     },
     "iPhone 12 Pro Max": {
         "screen": 220,
@@ -77,7 +86,8 @@ export const iPhoneInfos = {
         "back-glass": 180,
         "back-camera": 160,
         "id": 500,
-        "logic-board": 500
+        "logic-board": 500,
+        "RLP": 600
     },
     "iPhone 12 Pro": {
         "screen": 120,
@@ -85,7 +95,8 @@ export const iPhoneInfos = {
         "back-glass": 170,
         "back-camera": 130,
         "id": 450,
-        "logic-board": 450
+        "logic-board": 450,
+        "RLP": 550
     },
     "iPhone 12": {
         "screen": 120,
@@ -93,7 +104,8 @@ export const iPhoneInfos = {
         "back-glass": 120,
         "back-camera": 75,
         "id": 400,
-        "logic-board": 400
+        "logic-board": 400,
+        "RLP": 480
     },
     "iPhone 12 mini": {
         "screen": 150,
@@ -101,7 +113,8 @@ export const iPhoneInfos = {
         "back-glass": 130,
         "back-camera": 60,
         "id": 400,
-        "logic-board": 400
+        "logic-board": 400,
+        "RLP": 370
     },
     "iPhone 11 Pro Max": {
         "screen": 130,
@@ -109,7 +122,8 @@ export const iPhoneInfos = {
         "back-glass": 170,
         "back-camera": 110,
         "id": 400,
-        "logic-board": 400
+        "logic-board": 400,
+        "RLP": 400
     },
     "iPhone 11 Pro": {
         "screen": 100,
@@ -117,7 +131,8 @@ export const iPhoneInfos = {
         "back-glass": 160,
         "back-camera": 110,
         "id": 350,
-        "logic-board": 350
+        "logic-board": 350,
+        "RLP": 355
     },
     "iPhone 11": {
         "screen": 90,
@@ -125,7 +140,8 @@ export const iPhoneInfos = {
         "back-glass": 120,
         "back-camera": 75,
         "id": 300,
-        "logic-board": 300
+        "logic-board": 300,
+        "RLP": 300
     },
     "iPhone XR": {
         "screen": 80,
@@ -133,7 +149,8 @@ export const iPhoneInfos = {
         "back-glass": 70,
         "back-camera": 60,
         "id": 110,
-        "logic-board": 110
+        "logic-board": 110,
+        "RLP": 210
     },
     "iPhone XS Max": {
         "screen": 100,
@@ -141,7 +158,8 @@ export const iPhoneInfos = {
         "back-glass": 80,
         "back-camera": 75,
         "id": 100,
-        "logic-board": 100
+        "logic-board": 100,
+        "RLP": 360
     },
     "iPhone XS": {
         "screen": 100,
@@ -149,7 +167,8 @@ export const iPhoneInfos = {
         "back-glass": 80,
         "back-camera": 75,
         "id": 100,
-        "logic-board": 100
+        "logic-board": 100,
+        "RLP": 300
     },
     "iPhone X": {
         "screen": 80,
@@ -157,7 +176,8 @@ export const iPhoneInfos = {
         "back-glass": 70,
         "back-camera": 50,
         "id": 100,
-        "logic-board": 100
+        "logic-board": 100,
+        "RLP": 250
     },
     "iPhone 8 Plus": {
         "screen": 60,
@@ -165,7 +185,8 @@ export const iPhoneInfos = {
         "back-glass": 15,
         "back-camera": 80,
         "id": 15,
-        "logic-board": 125
+        "logic-board": 125,
+        "RLP": 150
     },
     "iPhone 8": {
         "screen": 20,
@@ -173,6 +194,11 @@ export const iPhoneInfos = {
         "back-glass": 10,
         "back-camera": 60,
         "id": 15,
-        "logic-board": 125
+        "logic-board": 125,
+        "RLP": 100
     }
 }
+
+// RLP = Refurbished lowest price (average)
+// back-glass includes the complete housing
+// id = logicboard because it has to be replaced together
